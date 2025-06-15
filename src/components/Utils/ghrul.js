638af508c0d11937e07cefc6f637834102cd6737
User01 <rigbykai1@gmail.com>
2025-06-14 21:25:01 -0600

    Urls fix

--- src/components/Utils/ghrul.js
+++ src/components/Utils/ghrul.js
@@ -1,5 +1,5 @@
 const ghUrl = (url) => {
-    return `https://raw.githubusercontent.com/Rigbykai1/Portfolio/master${url}`;
+    return `https://raw.githubusercontent.com/Rigbykai1/newPortfolio/main${url}`;
 }
 
 export default ghUrl
