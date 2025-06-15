6fb7746e5efa7b60d038457c8379843b42074843
User01 <rigbykai1@gmail.com>
2025-06-14 21:32:07 -0600

    Urls fix try 3

--- src/components/Globe/functions/GlobeFunctions.js
+++ src/components/Globe/functions/GlobeFunctions.js
@@ -6,7 +6,7 @@ export function addGlobePoints(sceneRef, specialPoints) {
     return new Promise((resolve) => {
         const radius = dotRadius();
         const img = new Image();
-        img.src = `${ghUrl}/src/Images/wmap.png`;
+        img.src = ghUrl('/src/Images/wmap.png');
         img.onload = () => {
             const { data, width, height } = extractImageData(img);
             const normalMesh = createInstancedDots(data, width, height, radius, 0.3, 0.2);
