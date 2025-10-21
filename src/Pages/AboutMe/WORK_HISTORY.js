49ee936ebfb398607315548b2959227830c87e9d
rigbykai <rigbykai1@gmail.com>
2025-10-20 22:02:02 -0600

    Correccion gramatical en /src/Pages/AboutMe/WORK_HISTORY.js

--- src/Pages/AboutMe/WORK_HISTORY.js
+++ src/Pages/AboutMe/WORK_HISTORY.js
@@ -13,22 +13,23 @@ export const WORK_HISTORY = [
         description: `Mi deber como operador mecánico era velar por la seguridad
             de los huéspedes en cada atracción, haciendo de su estadía un
             momento agradable y sobre todo seguro.
-            Aprendí mucho sobre al atención al cliente y sobre el
+            Aprendí mucho sobre la atención al cliente y sobre el
             funcionamiento de distintas atracciones.`,
         year: "2019 - 2020",
     },
     {
         title: "Super La Esquina",
         title1: "Dependiente",
-        description: `Debía atender clientes y facturar sus compras, también debía ingresar
-            facturas al sistema del local y gestionar pedidos a proveedores.`,
+        description: `Atendía a los clientes y gestionaba la facturación de sus compras. 
+        Además, registraba facturas en el sistema del local y recibia los pedidos con los proveedores.`,
         year: "2024 - 2024",
     },
     {
         title: "CER Evaluación de Riesgos",
-        title1: "Operario",
-        description: `Realizo mantenimiento y recarga a extintores, debo manipular distintas
-            máquinas y herramientas para la realización de estas labores.`,
+        title1: "Técnico en mantenimiento de equipos contra incendios",
+        description: `Realizo mantenimiento, recarga e instalación de extintores y sistemas de supresión de fuego (Fe36). 
+        Además, implementé un sistema de control en Notion para organizar mantenimientos e inventarios, mejorando la gestión interna. 
+        También brindo soporte técnico básico en oficina, configurando redes locales y optimizando tareas administrativas.`,
         year: "2024 - actualidad",
     },
 ];
